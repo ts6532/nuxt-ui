ceb6384098f5d8abf5d2173dc9b921049e03a5ac
Dmitry <papkovdmitry@gmail.com>
2025-08-10 12:23:15 +0300

    init

---
+++ app/components/FancyButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  color?: "red" | "blue";
+  label?: string;
+}
+
+const { color = "red", label = "fancyButton" } = defineProps<Props>();
+</script>
+
+<template>
+  <button>{{ label }}</button>
+</template>
+
+<style scoped>
+button {
+  background-color: v-bind(color);
+}
+</style>
